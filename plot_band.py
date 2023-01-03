807c34d5301177b1359c0a8c3803f14f8dc17542
Basavaraja G <basavaraja7371@gmail.com>
2023-01-03 18:43:12 +0530

    added plot_projband examplt to plot_band.py

--- plot_band.py
+++ plot_band.py
@@ -1,3 +1,9 @@
 import Bands as bnd
-band = bnd.Bands('silicon', 'BAND.dat', 'KLABELS', 7)
-band.bands_plot(-7, 5, 'blue', 1.5, 'png')
+
+# Bandstructure
+band = bnd.Bands('silicon', [['BAND.dat']], 'KLABELS', 7)
+band.plot_bands(-7, 5, 'blue', 1.5, 'png')
+
+# Projected Bands
+pband = bnd.Bands('silicon', [['PBAND_Si.dat',"s", "p_y", "p_x", "p_z"]], 'KLABELS', 7)
+pband.plot_projbands(ymin=-5,ymax=5,color='red',linewidth=1,save_as='png')
